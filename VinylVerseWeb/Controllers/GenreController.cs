97467a6187be4c84af69a5ae7fb3ba6d669b7d45
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-02-29 14:17:53 +0200

    BootstrapTheme_Imported

--- VinylVerseWeb/Controllers/GenreController.cs
+++ VinylVerseWeb/Controllers/GenreController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VinylVerseWeb.Data;
+using VinylVerseWeb.Models;
 
 namespace VinylVerseWeb.Controllers
 {
     public class GenreController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public GenreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Genre> genres = _context.Genres.ToList();
+            return View(genres);
         }
     }
 }
