df9c1b770f452c8fe94c59d6bff4e24d356c6df9
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-03-01 17:41:08 +0200

    Genre_Edit/Delete_FunctionalitiesImplemented

--- VinylVerseWeb/Controllers/GenreController.cs
+++ VinylVerseWeb/Controllers/GenreController.cs
@@ -36,6 +36,9 @@ namespace VinylVerseWeb.Controllers
             {
                 _context.Genres.Add(genre);
                 _context.SaveChanges();
+
+                TempData["success"] = "Genre added successfully";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -65,9 +68,45 @@ namespace VinylVerseWeb.Controllers
             {
                 _context.Genres.Update(genre);
                 _context.SaveChanges();
+
+                TempData["success"] = "Genre updated successfully";
+
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Genre? genre = _context.Genres.Find(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return View(genre);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Genre genreToDelete = _context.Genres.Find(id);
+            if(genreToDelete == null)
+            {
+                return NotFound();
+            }
+            _context.Genres.Remove(genreToDelete);
+            _context.SaveChanges();
+
+            TempData["success"] = "Genre deleted successfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
